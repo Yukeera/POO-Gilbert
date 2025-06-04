a338dfff11751daafcdb47f7ec90a5359b5edd18
Fernando Yuri <107281250+Yukeera@users.noreply.github.com>
2025-06-04 17:22:39 +0000

    Usando metodos json para fazer o crud da categoria

--- Aula_18/Categoria.py
+++ Aula_18/Categoria.py
@@ -1,3 +1,5 @@
+import json
+
 class Categoria:
     def __init__(self, id, d):
         self.setId(id)
@@ -18,3 +20,36 @@ class Categoria:
     def __str__(self):
         return f"Id = {self.__id}  Descrição = {self.__d}"
     
+
+class Categorias:
+    objetos = []
+
+    @classmethod
+    def inserir(cls):
+        pass
+    @classmethod
+    def listar(cls):
+        return cls.categoriaList
+    @classmethod
+    def listarId(cls, id):
+        for obj in cls.objetos:
+            if obj.id == id:
+                return obj
+            return None
+        
+    @classmethod
+    def atualizar(cls, obj):
+        x = cls.listarId(obj.id)
+        if (x != None):
+            cls.objetos.remove(x)
+            cls.objetos.append(obj)
+
+    @classmethod
+    def abrir(cls):
+        cls.objetos = []
+        try:
+            with open("categorias.json", mode = r) as arquivo:
+                s = json.load(arquivo)
+            
+        except FileNotFoundError:
+            pass
