0cd9897cb1820a287d43c181d7e0d7744efb7b1f
Fernando Yuri <107281250+Yukeera@users.noreply.github.com>
2025-07-10 03:24:25 +0000

    Criação da UI abrir conta

---
+++ Avaliação2/Templates/AbrirContaUI.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from views import View
+import time
+class AbrirContaUI:
+    def main():
+        st.header("Abrir Conta")
+        nome = st.text_input("Informe o seu nome")
+        email = st.text_input("Informe o seu email")
+        senha = st.text_input("Informe sua senha", type="password")
+        fone = st.text_input("informe seu Telefone")
+        if st.button("Criar conta!"):
+            View.cliente_inserir(nome, email, senha, fone)
+            st.success("Conta criada com sucesso.")
+            time.sleep(2)
+            st.rerun()
